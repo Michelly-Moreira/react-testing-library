3bf3901e78e0a8aadfc6a79020276a4b490e1741
Michelly <michelly.daiana@hotmail.com>
2022-11-22 16:23:32 -0300

    iniciando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+describe('Renderiza o App.js e...', () => {
+  describe('renderiza a home e...', () => {
+    test('verifica se os links "Home", "About" e "Favorite Pokémon" aparecem na tela', () => {
+      render(<App />); });
+  });
+});
