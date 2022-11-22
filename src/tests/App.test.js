e5e7b03e81521e4ac7211f46acd4a8750e7ea4a4
Michelly <michelly.daiana@hotmail.com>
2022-11-22 19:03:30 -0300

    task 01.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+// import { createMemoryHistory } from 'history';
+// import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
 
 describe('Renderiza o App.js e...', () => {
-  describe('renderiza a home e...', () => {
-    test('verifica se os links "Home", "About" e "Favorite Pokémon" aparecem na tela', () => {
-      render(<App />); });
+  describe('testa o componente App e...', () => {
+    test('verifica se os links "Home", "About" e "Favorite Pokémon" aparecem na tela e se ao clicar vão para as rotas correspondentes', () => {
+    /* duas formas de acessar com o router, abaixo usando o componente renderWithRouter:
+      const history = createMemoryHistory();
+       render(
+        <Router history={ history }>
+          <App />
+        </Router>,
+      ); */
+
+      const { history } = renderWithRouter(<App />);
+
+      // capturando o botão
+      const home = screen.getByRole('link', { name: /Home/i });
+      const about = screen.getByRole('link', { name: /About/i });
+      const favorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
+
+      expect(home).toBeInTheDocument();
+      expect(about).toBeInTheDocument();
+      expect(favorite).toBeInTheDocument();
+
+      userEvent.click(home); // interagindo
+      console.log(history.location.pathname); // history para acessar a sessão de histórico do navegador, pathname é onde está a rota.
+      expect(history.location.pathname).toBe('/'); // aferindo
+
+      userEvent.click(about);
+      expect(history.location.pathname).toBe('/about');
+
+      userEvent.click(favorite);
+      expect(history.location.pathname).toBe('/favorites');
+    });
+
+    test('renderiza a página de notFound ao entrar em URL desconhecida', async () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/nadaHaver');
+
+      const textNotFound = await screen.findByRole('heading', { name: 'Page requested not found' });
+      expect(textNotFound).toBeInTheDocument();
+    });
   });
 });
