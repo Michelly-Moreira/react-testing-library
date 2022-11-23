26030ed6e51ef5a104c1ece8f1189a655325cd4e
Michelly <michelly.daiana@hotmail.com>
2022-11-23 11:31:41 -0300

    task 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemon from '../pages/FavoritePokemon';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
@@ -17,14 +18,22 @@ describe('Renderiza o componente <FavoritePokemon/> e...', () => {
 });
 
 describe('Renderiza o componente <FavoritePokemon/> e...', () => {
-  test('verifica se todos os cards de Pokémon favoritados aparecem na tela', async () => {
-    // acessar
+  test('verifica se todos os cards de Pokémon favoritados aparecem na tela', () => {
     renderWithRouter(<App />);
+
     const moreDetails = screen.getByRole('link', { name: /more Details/i });
     expect(moreDetails).toBeInTheDocument();
     userEvent.click(moreDetails);
-    // const favorite = await screen.findAllByText('pokemon');
-    // aferir
+
+    const chooseFavorite = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    expect(chooseFavorite).toBeInTheDocument();
+    userEvent.click(chooseFavorite);
+
+    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
     expect(favorite).toBeInTheDocument();
+    userEvent.click(favorite);
+
+    const favoriteList = screen.getByText(/pikachu/i);
+    expect(favoriteList).toBeInTheDocument();
   });
 });
