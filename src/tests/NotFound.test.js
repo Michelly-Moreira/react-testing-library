48830a149e8bd89a96f4c8141baa34c913e97953
Michelly <michelly.daiana@hotmail.com>
2022-11-23 13:37:33 -0300

    task 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Renderiza o componente <NotFound /> e...', () => {
+  test('verifica se <h2/> com o texto "Page requested not found" aparece na tela', () => {
+    renderWithRouter(<NotFound />);
+    const alert = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    // console.log(alert);
+    expect(alert).toBeInTheDocument();
+  });
+  test('mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithRouter(<NotFound />);
+    const imageUrl = screen.getByRole('img', { name: /Pikachu crying/i });
+    // console.log(imageUrl.src);
+    expect(imageUrl.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
