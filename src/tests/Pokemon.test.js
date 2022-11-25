12475c64cc829459c5e0d579851ca9f4a5170625
Michelly <michelly.daiana@hotmail.com>
2022-11-25 20:06:08 -0300

    task 07

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,89 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const pokemonId = '/pokemon/25';
+const marked = 'Pikachu is marked as favorite';
+
+describe('Renderiza um card com informação de determinado Pokemon e...', () => {
+  test('será validado se renderizado o nome, tipo, peso e imagem corretos do pokémon', () => {
+    renderWithRouter(<App />);
+
+    const namePokemon = screen.getByText(/pikachu/i);
+    const typePokemon = screen.getByTestId('pokemon-type');
+    const averagePokemon = screen.getByText(/average weight: 6\.0 kg/i);
+    const pokemon = screen.getByRole('img', { name: /pikachu sprite/i });
+
+    expect(namePokemon).toBeInTheDocument();
+    expect(typePokemon).toHaveTextContent('Electric');
+    expect(averagePokemon).toBeInTheDocument();
+    expect(pokemon).toBeInTheDocument();
+    expect(pokemon.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokemon.alt).toBe('Pikachu sprite');
+  });
+
+  test('será validado se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes', () => {
+    renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /more details/i });
+    expect(details).toBeInTheDocument();
+    userEvent.click(details);
+  });
+
+  test('Ao clicar no link de navegação do Pokémon, redireciona a aplicação para a página de detalhes, será validado se a URL exibida no navegador muda para /pokemon/<id>', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /more details/i });
+    expect(details).toBeInTheDocument();
+
+    userEvent.click(details);
+    expect(history.location.pathname).toBe(pokemonId);
+  });
+
+  describe.only('Renderiza componente de estrela nos pokémons favoritados e...', () => {
+    test('será validado se renderizada uma imagem com o atributo src contendo o caminho /star-icon.svg', () => {
+      renderWithRouter(<App />);
+
+      const details = screen.getByRole('link', {
+        name: /more details/i });
+
+      userEvent.click(details);
+
+      const favorite = screen.getByRole('checkbox', {
+        name: /pokémon favoritado\?/i,
+      });
+      userEvent.click(favorite);
+
+      const favoriteLink = screen.getByRole('link', {
+        name: /favorite pokémon/i,
+      });
+      userEvent.click(favoriteLink);
+
+      const star = screen.getByRole('img', {
+        name: marked,
+      });
+
+      expect(star).toBeInTheDocument();
+      expect(star.src).toContain('/star-icon.svg');
+    });
+
+    test('será validado se a imagem tiver o atributo alt igual a <Pokemon> is marked as favorite', () => {
+      renderWithRouter(<App />);
+
+      const favoriteLink = screen.getByRole('link', {
+        name: /favorite pokémon/i,
+      });
+      userEvent.click(favoriteLink);
+
+      const star = screen.getByRole('img', {
+        name: marked,
+      });
+
+      expect(star).toBeInTheDocument();
+      expect(star.alt).toBe('Pikachu is marked as favorite');
+    });
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+// import PokemonDetails from '../pages/PokemonDetails';
+import App from '../App';
+
+describe('As informações detalhadas do Pokémon selecionado são mostradas na tela e...', () => {
+  test('será validado se é exibido na tela um h2 com o texto <name> Details', async () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(detailsLink);
+
+    const nameDetails = screen.getByRole('heading', {
+      name: /pikachu details/i,
+    });
+    expect(nameDetails).toBeInTheDocument();
+  });
+
+  test('será validado se é exibido na tela um h2 com o texto Summary', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(detailsLink);
+
+    const summary = screen.getByRole('heading', { level: 2, name: 'Summary' });
+    expect(summary).toBeInTheDocument();
+  });
+
+  test('será validado se é exibido na tela um resumo do <summary>', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(detailsLink);
+
+    const textSummary = screen.getByText(
+      /this intelligent pokémon roasts/i,
+    );
+    expect(textSummary).toBeInTheDocument();
+  });
+
+  describe('Todas as localizações do Pokémon devem ser mostradas na seção de detalhes e...', () => {
+    test('será validado se é exibido na tela um h2 com o texto Game Locations of <name>', () => {
+      renderWithRouter(<App />);
+      const detailsLink = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(detailsLink);
+
+      const locationGame = screen.getByRole('heading', {
+        name: /game locations of pikachu/i,
+      });
+      expect(locationGame).toBeInTheDocument();
+    });
+
+    test('será validado se são exibidas na tela imagens de localização com o src correto', () => {
+      renderWithRouter(<App />);
+      const detailsLink = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(detailsLink);
+
+      const locationForest = screen.getAllByRole('img', { name: 'Pikachu location' });
+
+      expect(locationForest[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+      expect(locationForest[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    });
+
+    test('será validado se exibido na tela uma label com o texto "Pokémon favoritado?" ', () => {
+      renderWithRouter(<App />);
+      const detailsLink = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(detailsLink);
+
+      const question = screen.getByText(/pokémon favoritado\?/i);
+      expect(question).toHaveTextContent('Pokémon favoritado?');
+    });
+  });
+});
